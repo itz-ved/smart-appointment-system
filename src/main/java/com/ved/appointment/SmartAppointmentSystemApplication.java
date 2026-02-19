3ced48d4d0339d97cbd5b0661774c4a026b9f2bc
Ved <mishraved492@gmail.com>
2026-02-19 14:33:50 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/ved/appointment/SmartAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ved.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartAppointmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ved/appointment/SmartAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ved.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAppointmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
